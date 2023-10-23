9f6d6413c294e305f97deed04500a2cfacbc64ce
colossalg <colossalg@Ubuntu-22.10.myguest.virtualbox.org>
2023-10-23 21:27:41 +0800

    Fix tabs.

--- test/framework.c
+++ test/framework.c
@@ -7,21 +7,21 @@
 
 void run_test_in_own_process(const char* name, void (*test)())
 {
-	fprintf(stderr, "Running test %s...\n", name);
+    fprintf(stderr, "Running test %s...\n", name);
 
-	pid_t pid = fork();
-	if (-1 == pid) {
-		fprintf(stderr, "Failure: Was unable to fork process for test.\n");
-	} else if (0 == pid) {
-		test();
-		exit(EXIT_SUCCESS);
-	} else {
-		int status = 0;
-		wait(&status);
-		if (0 == status) {
-			fprintf(stderr, "Success.\n");
-		} else {
-			fprintf(stderr, "Failure: Process exited with code %i.\n", status);
-		}
-	}
+    pid_t pid = fork();
+    if (-1 == pid) {
+        fprintf(stderr, "Failure: Was unable to fork process for test.\n");
+    } else if (0 == pid) {
+        test();
+        exit(EXIT_SUCCESS);
+    } else {
+        int status = 0;
+        wait(&status);
+        if (0 == status) {
+            fprintf(stderr, "Success.\n");
+        } else {
+            fprintf(stderr, "Failure: Process exited with code %i.\n", status);
+        }
+    }
 }
--- test/framework.h
+++ test/framework.h
@@ -2,7 +2,7 @@
 #define COLOSSALG_TESTING_FRAMEWORK_H
 
 #define RUN_TEST_IN_OWN_PROCESS(test_name) \
-	run_test_in_own_process(#test_name "()", &test_name);
+    run_test_in_own_process(#test_name "()", &test_name);
 
 void run_test_in_own_process(const char* name, void (*test)());
 
